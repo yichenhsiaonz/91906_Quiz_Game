f8bcf165afb43947ec5aba587e5d2a20769b2853
hsiaoy71950 <51727091+hsiaoy71950@users.noreply.github.com>
2021-06-01 14:02:10 +1200

    haha yes

--- 10_Component_Regex_String_Checker.py
+++ 10_Component_Regex_String_Checker.py
@@ -1,2 +1,15 @@
 import re
 
+
+def regex_check(file_name):
+    if re.search("[.<>:\"/|?*\\\\\040]", file_name):
+        return "Invalid file name - illegal character(s)(. < > : \" / \\ | ? * )"
+    if file_name == "":
+        return "Invalid file name - can't be blank"
+    return "No Error"
+
+loop = ""
+while loop == "":
+    user_input = input("enter a filename without a extension: ")
+    output = regex_check(user_input)
+    print(output)
